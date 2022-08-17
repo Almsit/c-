86eaa521c21ead99df2c2d3624fe532fc4563250
«Almsit» <ZulAman@yandex.ru>
2022-08-17 19:55:15 +0300

    create if

---
+++ if/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя");
+string username = Console.ReadLine();
+if(username.ToLower() == "1"){
+    Console.WriteLine(username);
+    Console.WriteLine("да");
+} else {
+    Console.WriteLine(username);
+    Console.WriteLine("нет");
+}
