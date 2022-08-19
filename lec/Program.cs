ef5ae552b99de9bc0ed22d8fc1208d42a49d5a92
«Almsit» <ZulAman@yandex.ru>
2022-08-19 10:11:27 +0300

    create lec

---
+++ lec/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.Write(result);
